d3cc4e5315576f06fc52fb4def569ae0c9b34646
Khushwant Singh Rathore <51788179+khushwantrathore7865gmailcom@users.noreply.github.com>
2020-12-03 17:48:50 +0530

    Add files via upload

---
+++ libraryProject/src/libraryproject/conn.java
@@ -0,0 +1,20 @@
+package libraryproject;
+
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///project?useSSL=false","root","1234");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
